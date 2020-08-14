c20c82ca4a2981c00a81052ecaac25999ec41f01
RenanSS1 <renansantos009@gmail.com>
2020-08-14 20:55:02 -0300

    Subindo aula da NWL-2 para o Git

---
+++ src/server.js
@@ -0,0 +1,25 @@
+// servidor
+const express = require('express')
+const server = express()
+
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses
+} = require('./pages')
+
+//config nunjuks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+server
+// configurar arquivos estaticos (css, scripts, imagens)
+.use(express.static("public"))
+//rotas da aplicação
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.listen(5500)
